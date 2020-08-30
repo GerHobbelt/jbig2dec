9b352a9e7ecb67e8055b351f9071ddec08dbfc66
Ger Hobbelt <ger@hobbelt.com>
2020-08-30 16:55:39 +0200

    mark suspect free() line in the code with //////// -- trouble is though that the crashing mujstest commands DO NOT access this line before failing dramatically in a heap check error.   :-S

--- jbig2_text.c
+++ jbig2_text.c
@@ -404,7 +404,7 @@ cleanup1:
                 }
 
                 if (code1 < 0 || code2 < 0 || code3 < 0 || code4 < 0 || code5 < 0 || code6 < 0) {
-                    jbig2_image_release(ctx, IB);
+                    jbig2_image_release(ctx, IB); ////////////////////
                     code = jbig2_error(ctx, JBIG2_SEVERITY_WARNING, segment->number, "failed to decode data");
                     goto cleanup2;
                 }
